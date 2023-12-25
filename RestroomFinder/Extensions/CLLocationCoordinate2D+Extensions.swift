e7ddaa64b9f90655ff337c07f96905933ad43a89
Tom Montgomery <montgomery3681@gmail.com>
2023-12-24 22:37:07 -0700

    - beginnings of CLMonitoring, switch to starter project

---
+++ RestroomFinder/Extensions/CLLocationCoordinate2D+Extensions.swift
@@ -0,0 +1,10 @@
+//
+//  CLLocationCoordinate2D+Extensions.swift
+//  RestroomFinder
+//
+//  Created by tom montgomery on 12/23/23.
+//
+
+import Foundation
+
+// switched over to the starter project
